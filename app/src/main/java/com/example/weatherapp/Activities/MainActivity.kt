d132086150f6252b88948c792ec096bf2280570a
zelshenhab <z.elshenhab2012@gmail.com>
2023-03-30 10:21:00 +0300

    initial commit

--- app/src/main/java/com/example/weatherapp/Activities/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/Activities/MainActivity.kt
@@ -1,12 +1,63 @@
 package com.example.weatherapp.Activities
 
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingUtil
 import com.example.weatherapp.R
+import com.example.weatherapp.databinding.ActivityMainBinding
+import com.google.android.gms.location.CurrentLocationRequest
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var binding: ActivityMainBinding
+
+    private lateinit var currentLocation: Location
+
+    private lateinit var fusedLocationProvider:FusedLocationProviderClient
+
+    private val LOCATION_REQUEST_CODE=101
+
+    private val apiKey="f70ca239bf30695349b25a9bb3361c69"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        fusedLocationProvider=LocationServices.getFusedLocationProviderClient(this)
+
+        getCurrentLocation()
+
+        binding.citySearch.setOnEditorActionListener { textView, i, keyEvent ->
+
+            if (i==EditorInfo.IME_ACTION_SEARCH){
+                getCityWeather(binding.citySearch.text.toString())
+
+                val view=this.currentFocus
+
+                if (view!=null){
+
+                    val imm:InputMethodManager= getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+
+                    imm.hideSoftInputFromWindow(view.windowToken,0)
+
+
+                }
+
+
+            }
+
+
+        }
+
+
+
     }
 }
--- app/src/main/java/com/example/weatherapp/Utilites/Apilnterface.kt
+++ app/src/main/java/com/example/weatherapp/Utilites/Apilnterface.kt
@@ -24,4 +24,6 @@ interface Apilnterface {
 
 
 
+
+
 }
